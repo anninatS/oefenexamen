94d111c88463e7e604abce0a43e1d0d629136255
anninatS <126657077+anninatS@users.noreply.github.com>
2025-02-25 12:33:15 +0100

    migartions

---
+++ database/migrations/2025_02_25_112010_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('type');
+            $table->string('rarity');
+            $table->integer('strength');
+            $table->integer('speed');
+            $table->integer('durability');
+            $table->text('magic_properties');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
